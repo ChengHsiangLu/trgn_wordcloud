2084abc8f2edb5c1c6448ce28a4a7fac291f2c6e
ChengHsiangLu <clu74108@usc.edu>
2021-09-17 13:31:53 -0700

    done

--- generate_wordcloud_from_file.sh
+++ generate_wordcloud_from_file.sh
@@ -2,13 +2,11 @@
 
 count=1
 while IFS= read -r line; do
-    wget -O "./current_pages/file${count}.html" $line
-    html2text.py current_pages/file${count}.html >> my_current.txt
+    wget -O "/home/clu74108/trgn_wordcloud/current_pages/file${count}.html" $line
+    ~/bin/html2text.py /home/clu74108/trgn_wordcloud/current_pages/file${count}.html >> /home/clu74108/trgn_wordcloud/my_current.txt
     ((count++))
 done
 
 
-wordcloud_cli --text my_current.txt --imagefile bbccnn.png
-
-mv /home/clu74108/trgn_wordcloud/my_current.txt /home/clu74108/trgn_wordcloud/current_pages
-mv /home/clu74108/trgn_wordcloud/bbccnn.png /home/clu74108/public_html
+~/.local/bin/wordcloud_cli --text /home/clu74108/trgn_wordcloud/my_current.txt --imagefile /home/clu74108/trgn_wordcloud/bbccnn.png
+mv /home/clu74108/trgn_wordcloud/bbccnn.png /home/clu74108/public_html/
